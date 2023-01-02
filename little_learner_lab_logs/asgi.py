e04ea4db912ca49a07e148040902b6bf9e515163
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-02 12:51:35 +0000

    Initial Commit Django installation and setup

---
+++ little_learner_lab_logs/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for little_learner_lab_logs project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_learner_lab_logs.settings')
+
+application = get_asgi_application()
