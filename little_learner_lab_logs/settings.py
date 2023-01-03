785024d7272cd3103d898d324c2941eb6d678162
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-03 08:36:16 +0000

    Fixed indentation errors iand added py version <3.9 again

--- little_learner_lab_logs/settings.py
+++ little_learner_lab_logs/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'little_learner_lab_logs.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DATABASES = {
- #     'default': {
- #         'ENGINE': 'django.db.backends.sqlite3',
- #         'NAME': BASE_DIR / 'db.sqlite3',
- #     }
- # }
-    
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
