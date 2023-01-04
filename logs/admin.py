c303ee3535246cf3bada917d49db942c66eda5d6
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-04 22:07:47 +0000

    Finish setting up admin site and create files to set up views

--- logs/admin.py
+++ logs/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+# Admin page functionality for posts
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('description', 'items_required', 'steps_to_perform')
+    list_filter = ('created_on', 'status')
+    search_fields = ['title', 'body']
+    summernote_fields = ('body')
+
+
+# Comments display on admin site
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
