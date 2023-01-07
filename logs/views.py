b603f3304439da084b3588427b3fd9c6d690034c
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-07 23:23:42 +0000

    Add views for post detail and comments

--- logs/views.py
+++ logs/views.py
@@ -11,13 +11,29 @@ class CreatePost(generic.CreateView):
 
 
 class PostList(generic.ListView):
+    ''' Class to show list of posts'''
     model = Post
     queryset = Post.objects.filter(status=1).order_by('created_on')
     template_name = 'index.html'
     paginate_by = 6
 
 
-class PostDetail(generic.DetailView):
+class PostDetail(View):
     ''' Class to show single posts in a detail view '''
-    model = Post
-    template_name = 'log_details.html'
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.user_comments.order_by('-created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "log_details.html",
+            {
+                "post": post,
+                "comments" : comments,
+                "liked" : liked,
+            }
+        )
