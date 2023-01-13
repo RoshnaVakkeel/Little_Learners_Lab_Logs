9e6c721318d3a3e9dc63ec3154bd62ea554f6b61
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-13 18:37:25 +0000

    Comments message fix

--- logs/views.py
+++ logs/views.py
@@ -53,34 +53,28 @@ class PostDetail(View):
         )
 
     def post(self, request, slug, *args, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.user_comments.order_by('-created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+        if request.user.is_authenticated:
+            queryset = Post.objects.filter(status=1)
+            post = get_object_or_404(queryset, slug=slug)
+            comments = post.user_comments.order_by('-created_on')
+            liked = False
+            if post.likes.filter(id=self.request.user.id).exists():
+                liked = True
 
-        comment_form = CommentForm(data=request.POST)
+            comment_form = CommentForm(data=request.POST)
 
-        if comment_form.is_valid():
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
+            if comment_form.is_valid():
+                comment_form.instance.name = request.user.username
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.save()
+                messages.success(request, 'Thank you for your comment!')
+            else:
+                comment_form = CommentForm()
 
-        return render(
-            request,
-            "log_details.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "liked": liked,
-                "comment_form": CommentForm(),
-            }
-        )
+            # User HttpResponseRedirect here instead of render to ensure comment
+            # is not re-submitted on page re-load
+            return HttpResponseRedirect(reverse('log-details', args=[slug]))
 
 
 class SharedPostsByUsers(LoginRequiredMixin, generic.ListView):
