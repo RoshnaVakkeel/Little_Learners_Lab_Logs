d7b02212c49f55d6906a2a36f5fd8c39e51c4bc7
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-03-13 11:01:30 +0000

    Set debug to True in settings

--- little_learner_lab_logs/settings.py
+++ little_learner_lab_logs/settings.py
@@ -28,9 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['little-learners-lab-logs.herokuapp.com', 'localhost', '*']
 
